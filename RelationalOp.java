b2664413ae89994f307834cae3e49531946e7b0e
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-05 16:25:12 +0900

    Add files via upload

---
+++ RelationalOp.java
@@ -0,0 +1,14 @@
+package java_area;
+
+public class RelationalOp {
+
+	public static void main(String[] args) {
+
+		System.out.println("3 >= 2 : " + ( 3 >= 2 ));
+		System.out.println("3 <= 2 : " + ( 3 <= 2 ));
+		System.out.println("3 == 2 : " + ( 3 == 2 ));
+		System.out.println("3 != 2 : " + ( 3 != 2 ));
+		
+	}
+
+}
