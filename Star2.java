5845fa5befcb849c3b71b298581c688d55f8888c
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-08 17:56:25 +0900

    Add files via upload
    별찍기

---
+++ Star2.java
@@ -0,0 +1,23 @@
+
+public class Star2 {
+
+	public static void main(String[] args) {
+		
+		for (int i=0; i<= 3; i++) {
+			for (int j=0; j<=4; j++) {
+			System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
+}
+
+/*
+
+*****
+*****
+*****
+***** 
+
+*/
---
+++ Star3.java
@@ -0,0 +1,27 @@
+
+public class Star3 {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i<= 5; i++) {
+			for (int j=1; j<=i-1; j++) {
+			System.out.print(" ");
+			}
+			for (int j=i; j<=5; j++) {
+				System.out.print("*");
+				}
+
+			System.out.println();
+		}
+	}
+}
+
+/*
+
+*****
+ ****
+  ***
+   **
+    *
+
+*/
