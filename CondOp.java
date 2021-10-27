6fb708b6c6b184d973292177e95a94ee9c0009e3
seyoon <91830706+tora-ng@users.noreply.github.com>
2021-10-27 17:21:18 +0900

    10/27

--- CondOp.java
+++ CondOp.java
@@ -1,7 +1,8 @@
-package class_10_27;
+package day_10_27;
 
 interface AreaGetable {
 	double getArea();
+
 }
 
 class Circle implements AreaGetable {
@@ -31,54 +32,34 @@ class Rectangle implements AreaGetable {
 	public double getArea() {
 		return width * height;
 	}
-
 }
 
 public class CondOp {
 
+	public static void main(String[] args) {
+		AreaGetable[] area = { new Rectangle(4, 5), new Circle(4), new Circle(4), new Circle(5), new Circle(5),
+				new Circle(6), new Rectangle(4, 5), new Rectangle(4, 5), new Rectangle(4, 5), };
+
+		// AreaGetable oneArea = new Circle(4);
+		System.out.println(getAllArea(area));
+		System.out.println(getArea(new Circle(10))); // 314
+		System.out.println(getArea(new Rectangle(4, 5))); // 20
+
+		// oneArea = new Rectangle(4,5);
+		// System.out.println(area.getArea());
+
+	}
+
 	public static double getAllArea(AreaGetable[] area) {
 		double sum = 0;
-
-		for (AreaGetable areaGetable : area) {
-			sum = sum + areaGetable.getArea();
-		}
 
 		for (int i = 0; i < area.length; i++) {
 			sum = sum + area[i].getArea();
 		}
-
 		return sum;
-
 	}
 
-	public static void main(String[] args) {
-		AreaGetable[] area = { new Rectangle(4, 5), new Circle(4) };
-
-		// AreaGetable oneArea = new Circle(4);
-		System.out.println(getAllArea(area)); // 함수가 main에 속해야 함
-		System.out.println(getArea(new Circle(10))); // 314
-		System.out.println(getArea(new Rectangle(4, 5))); // 320
-
-		// oneArea = new Rectangle(4,5);
-		// System.out.println(area.getArea());
-	}
-
-	
-	
-	// 함수 오버라이딩 사용해서
-	private static double getArea(AreaGetable area) {
+	public static double getArea(AreaGetable area) {
 		return area.getArea();
 	}
-
-	
-	
-//	private static double getArea(Circle circle) {
-//		return circle.getArea();
-//	}
-//
-//	private static double getArea(Rectangle rectangle) {
-//		
-//		return rectangle.getArea();
-//	}
-
 }
